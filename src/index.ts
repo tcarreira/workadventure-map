b79514f5982ce511442908680862f4f8dbebeccc
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-03-11 15:42:11 -0300

    update map

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,30 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
+const popups: any = [];
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
+
+    popups.mainRoom = WA.ui.openPopup("mainRoom","Welcome to UBUCON!",[]);
+    popups.room1 = WA.ui.openPopup("boardRoom1","Ubuntu installation workshop",[]);
+    let i = 0;
+
+    window.setInterval(function(){
+        i++;
+        popups.room1.close();
+
+        if (i%2==0){
+            popups.room1 = WA.ui.openPopup("boardRoom1","Installing something",[]);
+        } else {
+            const today = new Date();
+            const t = new Date(today.getTime() + 5*60);
+            popups.room1 = WA.ui.openPopup("boardRoom1","Next talk at "+t.toLocaleTimeString(),[]);
+        }
+    }, 20000);
+
 
     WA.room.onEnterLayer('clockZone').subscribe(() => {
         const today = new Date();
